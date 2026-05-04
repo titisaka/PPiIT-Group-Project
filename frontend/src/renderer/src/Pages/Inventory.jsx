6fe7f97cf7e9defa0ce2497e68c14ecd308e5637
MGormley <matthewgormley005@gmail.com>
2026-05-04 20:51:58 +0100

    Inventory Pages updates

---
+++ frontend/src/renderer/src/Pages/Inventory.jsx
@@ -0,0 +1,16 @@
+import "../styles.css"
+
+export default function Inventory() {
+  //Import all
+  return <div className="inventory-table">
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+    <div>Products</div>
+    <div>Inventory</div>
+    <div>Cashier</div>
+    <div></div>
+    <div></div>
+  </div>
+}
+
